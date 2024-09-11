33d88c5433f7bede08c5545f693daf4dd30f5779
wightkid <103604852+wightkid@users.noreply.github.com>
2024-09-10 21:45:40 -0400

    Add player vars

--- callaway-brandon-a1-tbgame/Program.cs
+++ callaway-brandon-a1-tbgame/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// PERSONAL TESTING SANDBOX, NOT ACTUAL ASSIGNMENT
+
+// Player variables
+string player_name = "TEST";   //Player character name
+int player_health = 100;       //Player health
+int player_action_points = 0;  //Player Action Points
+int player_luck = 0;           //Player Luck
+int player_melee_damage = 10;  //Player melee damage
+int player_ranged_damage = 9;  //Player ranged damage
+int player_gold = 0;           //Player gold
+string player_equipped_trinket = "TESTRINKET"; //Player's currently Equipped Trinket
+int player_room_count = 0;     //Rooms player has entered
+
+string player_class = "TESTCLASS"; //Player Class
+
+
+
+
+
+
+
+
+
+
+
